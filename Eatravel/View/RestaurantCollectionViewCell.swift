d770efe5dbaea89c1efde5bb815c5f9ebdb9248b
BurhanDundar <57062662+BurhanDundar@users.noreply.github.com>
2023-10-14 21:10:40 +0300

    .

--- Eatravel/View/RestaurantCollectionViewCell.swift
+++ Eatravel/View/RestaurantCollectionViewCell.swift
@@ -10,7 +10,8 @@
 import UIKit
 
 class RestaurantCollectionViewCell: UICollectionViewCell {
-     
+    
+    
     @IBOutlet weak var restaurantImageView: UIImageView!
     @IBOutlet weak var restaurantNameLabel: UILabel!
     @IBOutlet weak var restaurantRankLabel: UILabel!
